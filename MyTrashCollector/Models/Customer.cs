a8cc67066d1c2beefe73e221d1523b48df0fc6c5
Hannah Bingham <binghan.hb@gmail.com>
2019-10-02 09:45:14 -0500

    Created database for employee and customer.

---
+++ MyTrashCollector/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyTrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int  ZipCode { get; set; }
+        public double Balance { get; set; }
+        
+    }
+}
--- MyTrashCollector/Models/IdentityModels.cs
+++ MyTrashCollector/Models/IdentityModels.cs
@@ -19,7 +19,9 @@ namespace MyTrashCollector.Models
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
+    {   
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
